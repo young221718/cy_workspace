1ad9e56b4fe32bcf0f98df3450a271b86662eeb8
cy_titan2 <young221718@gmail.com>
2024-06-20 11:21:38 +0900

    test diffusion model

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-a = 10 + 5
-b = 20 + 5
-c = 30 + 5
+from huggingface_hub import login
+login(token='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')
+
+import torch
+from diffusers import StableDiffusion3Pipeline
+
+pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3-medium-diffusers", torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+image = pipe(
+    "Draw a building exterior with a traditional dial design that has a solid, safe-like feel.",
+    negative_prompt="",
+    num_inference_steps=28,
+    guidance_scale=7.0,
+).images[0]
+
+image.save("output.png")
