80e510e15dd1ba96fd2d922460aa47afe1745784
CY-titan1 <young221718@gmail.com>
2024-07-09 13:01:03 +0900

    let's start to prepare conference

--- rtdetr/src/zoo/cydetr/__init__.py
+++ rtdetr/src/zoo/cydetr/__init__.py
@@ -4,10 +4,8 @@
 from .rtdetr import *
 
 from .hybrid_encoder import *
-from .efficient_encoder import *
 from .rtdetr_decoder import *
 from .rtdetr_postprocessor import *
 from .rtdetr_criterion import *
-from .rtdetr_auxloss import *
 
 from .matcher import *
